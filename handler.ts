779055c016b75d01c8517f51013cdd2c5ec9831f
Tyler Shin <shincode@icloud.com>
2020-06-09 20:20:35 +0900

    set

--- handler.ts
+++ handler.ts
@@ -1,17 +1,38 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import {
+  puppeteer,
+  args,
+  executablePath,
+  defaultViewport,
+  headless,
+} from "chrome-aws-lambda";
 import "source-map-support/register";
 
-export const crawl: APIGatewayProxyHandler = async (event, _context) => {
+export const crawl: APIGatewayProxyHandler = async (_event, _context) => {
+  const browser = await puppeteer.launch({
+    args,
+    defaultViewport,
+    executablePath: await executablePath,
+    headless,
+    ignoreHTTPSErrors: true,
+  });
+
+  const page = await browser.newPage();
+  page.setViewport({ width: 1920, height: 1080 });
+  page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
+  );
+  await page.goto("https://google.conm", { waitUntil: "networkidle0" });
+
+  const title = await page.title();
+  const content = await page.content();
+  console.log(title);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message:
-          "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      ["Content-Type"]: "text/html; charset=utf-8",
+    },
+    body: content,
   };
 };
